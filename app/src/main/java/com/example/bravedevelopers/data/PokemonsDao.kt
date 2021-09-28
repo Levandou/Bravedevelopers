4216d3268e80691b003a34130722f3592ee06ef2
Levandou <task65apps>
2021-09-28 19:53:19 +0400

    Changing the database

--- app/src/main/java/com/example/bravedevelopers/data/PokemonsDao.kt
+++ app/src/main/java/com/example/bravedevelopers/data/PokemonsDao.kt
@@ -1,17 +1,21 @@
 package com.example.bravedevelopers.data
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.example.bravedevelopers.domain.InformationAboutPokemon
 
 
 @Dao
 interface PokemonsDao {
-@Query("SELECT * FROM name_list ORDER BY name ")
+/*@Query("SELECT * FROM name_list ORDER BY name ")
     fun getNameList():LiveData< List <InformationAboutPokemon> >
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertNames(resultList: InformationAboutPokemon)
+  */
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertFavorite(informationAboutPokemon: InformationAboutPokemon)
+    @Delete
+    fun deleteFavorite(informationAboutPokemon: InformationAboutPokemon)
+
 }
---
+++ app/src/main/java/com/example/bravedevelopers/data/RepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.example.bravedevelopers.data
+
+import com.example.bravedevelopers.domain.InformationAboutPokemon
+import com.example.bravedevelopers.domain.useCase.Repository
+
+object RepositoryImpl:Repository {
+    override fun addToFavorites(informationAboutPokemon: InformationAboutPokemon) {
+        TODO("Not yet implemented")
+    }
+
+    override fun randomSearch(informationAboutPokemon: InformationAboutPokemon) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteFromFavorites(informationAboutPokemon: InformationAboutPokemon) {
+        TODO("Not yet implemented")
+    }
+}
